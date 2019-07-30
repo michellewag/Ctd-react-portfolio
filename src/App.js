97b407c1948bb3ea9c73d649b05e82c49c70bc60
Ubuntu <ubuntu@ip-172-31-83-210.ec2.internal>
2019-07-30 17:48:55 +0000

    completed project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 
 
 
-
-//this part works good  
-
 class App extends React.Component {
     render() {
            return (
@@ -14,65 +11,34 @@ class App extends React.Component {
       <p>Name: {this.props.myName}</p>
       <p>Organization: {this.props.myOrganization}</p>
       <p>Instructor: {this.props.myInstructor}</p>
-    </div>
-   );
-  }
+      <h1>Beginner Class</h1>
+      <p>{beginnerClass}</p>
+      <h2>Intermediate Class</h2>
+      <p>{intermediateClass}</p>
+      <h3>Advanced Class</h3>
+      <p>{advancedClass}</p>
+     </div>
+     );
+   }
  }
 
-//fix up this class component
-
-  class Moreinfo extends React.Component {
-    render() {
-          return (
-    <div className = "MoreInfo">
-    <h1>Beginner Class</h1>
-        <p>We learned many topics in this class.</p>
-        <p>They are listed below:{this.props.beginnerClass}</p>
-    </div>
- );
- }
-}
-  
-     
-  
-// hard copy what we learned first then call it in a component 
 
 let beginnerClass = [
-    "directories", "abstractions", "JavaScript Operators", 
-    "variables", "arrays", "objects", "prototypes",
-    "if/then statements, for loops, while loops",
-    "functions", "factory functions", "key/value pairs"
-    ];
+    "Directories,", "  abstractions,", "  data types,", "  JavaScript operators,", 
+    "  variables,", "  arrays,", "  objects,", "  prototypes,",
+    "  if/then statements,", "  for loops,", "  while loops,",
+    "  functions,", "  factory functions,", "  key/value pairs"
+   ];
     
 let intermediateClass = [
-    "scope", "properties", "methods", "npm packages",
-    "test driven development", "Git/GitHub, HTML/CSS", 
+    "Scope,", "  properties,", "  methods,", "  npm packages,",
+    "  test driven development,", "  Git/GitHub,", "  HTML/CSS", 
     ];
     
 let advancedClass = ["React"];
     
   
   
- // ----  IDEAS----- 
-  // {props.beginnerClass}               
-                
-   
- //  function render() {
-  //     ReactDOM.render(<App />, document.getElementById('root'));
- //  }
-   
- //  render();
- //     <ClassInfo beginnerClass = {beginnerClass} />,
-      
-      
-      
-  
-  
-  
-  
-  
+ 
 
-
-
-
-export default App;
+export default App; 
